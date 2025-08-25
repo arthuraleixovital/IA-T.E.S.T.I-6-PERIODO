4c1f52f3904ac3d6d0c5023be3418b0d2b937f6d
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-08-25 10:55:12 -0400

    Aula 25/08/2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScenarioModule } from './modules/scenario/scenario.module';
 
 @Module({
-  imports: [],
+  imports: [ScenarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
